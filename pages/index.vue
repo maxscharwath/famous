1d24433f29a4954a5cb9a8f9314c634566b51e1c
Maxime Scharwath <maxscharwath@gmail.com>
2022-10-02 19:02:11 +0200

    infinite background

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Background />
+  <div
+    class="z-10 mx-5 flex h-screen flex-col items-center justify-center"
+  >
+    <div
+      key="container"
+      class="w-full max-w-4xl rounded-lg border-2 border-gray-200/50 bg-gray-200/90 p-10 shadow-2xl backdrop-blur-sm"
+    >
+      <input
+        type="text"
+        class="w-full rounded-lg border-2 border-gray-300 p-4"
+        placeholder="Enter your username"
+      >
+    </div>
+  </div>
+</template>
